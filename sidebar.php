c7f935e858b24d21740e4507c3a88c4bc4127f33
Alexandre Marques <alxmqsjr@gmail.com>
2021-07-25 21:01:25 -0300

    origin

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,25 @@
 <div class="col-md-4 bb-col-link">
 
-    <?php 
-        if ( is_active_sidebar( 'bb-sidebar' ) ) {
-            dynamic_sidebar( 'bb-sidebar' );
-        } 
-    ?>
+    <?php do_action( 'before_sidebar' ); ?>
+
+    <?php if (! dynamic_sidebar( 'bb-sidebar' ) ) : ?>
+
+        <div class="card my-4">
+            <h5 class="card-header"><?php esc_html_e( 'Pesquisar', 'bb' ); ?></h5>
+            <?php get_search_form(); ?>
+        </div>
+
+        <div class="card my-4 bb-archive">
+            <h5 class="card-header"><?php esc_html_e( 'Arquivos', 'bb' ); ?></h5>
+            <ul>
+                <?php
+                    $archives_args = array( 'type' => 'monthly' );
+                    wp_get_archives( $archives_args );
+                ?>
+            </ul>
+        </div>
+
+    <?php endif; ?>
+
 
 </div>
